547c0fed8dd2f4fabb306c703e645a91f6431727
Jeff Palladino <jpalladino@monetate.com>
2015-01-08 14:47:32 -0500

    Added game menu
    Game state: Playable

--- Game.py
+++ Game.py
@@ -58,24 +58,83 @@ tdl.setFont('terminal8x8_gs_ro.png')  # Configure the font.
 # Create the root console.
 console = tdl.init(SCREEN_WIDTH, SCREEN_HEIGHT, 'Roguelike Game')
 
-action_log = tdl.Console(40, 15)
-action_log.move(0, 1)
-action_log.setMode('scroll')
-
-action_log.printStr('Welcome,\n\nYour goal is to find the\nCone of Dunshire (!).\n\n' +
-                     'Use Caution as there are Trolls (T)\nand Orcs (o) lurking in ' +
-                     'this dungeon!\n\n')
-
-status_panel = tdl.Console(40, 15)
+level_manager = None
+level = None
+dungeon = None
+player = None
+action_log = None
+status_panel = None
 
 
-level_manager = LevelManager()
-level = level_manager.createLevel()
-dungeon = level_manager.dungeon
-player = dungeon.player
+def show_main_menu():
+    menu_options = ['Play a new game', 'Quit']
+
+    main_menu = tdl.Console(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    menu(main_menu, menu_options)
+    console.blit(main_menu, 0, 0)
+
+    tdl.flush()
+
+    key_event = tdl.event.keyWait()
+
+    if key_event.keychar.upper() == 'A':
+        new_game()
+        play_game()
+    elif key_event.keychar.upper() == 'B':
+        # Halt the script using SystemExit
+        raise SystemExit('The window has been closed.')
+
+
+def menu(console, options):
+
+    if len(options) > 26:
+        raise ValueError('Cannot have a menu with more than 26 options.')
+
+    x = 20
+    y = 20
+
+    print_str(console, 'Welcome', x, y)
+    y += 2
+    print_str(console, 'Your goal is to find the Cone of Dunshire (!).', x, y)
+    y += 2
+    print_str(console, 'Use Caution as there are Trolls (T)and Orcs (o)', x, y)
+    y += 1
+    print_str(console, 'lurking in this dungeon!', x, y)
+    y += 2
+
+    letter_index = ord('a')
+    x += 5
+    for option_text in options:
+        text = '(' + chr(letter_index) + ') ' + option_text
+        print_str(console, text, x, y)
+        y += 2
+        letter_index += 1
+
+
+def print_str(console, string, x, y):
+    console.move(x, y)
+    console.printStr(string)
+    console.move(x, y)
+
+
+def new_game():
+    global level_manager, level, dungeon, player, action_log, status_panel
+
+    level_manager = LevelManager()
+    level = level_manager.createLevel()
+    dungeon = level_manager.dungeon
+    player = dungeon.player
+
+    action_log = tdl.Console(40, 15)
+    action_log.move(0, 1)
+    action_log.setMode('scroll')
+
+    status_panel = tdl.Console(40, 15)
 
 
 def isTransparent(x, y):
+
     try:
         if dungeon.map[x][y].block_sight and dungeon.map[x][y].blocked:
             return False
@@ -111,54 +170,60 @@ def render_all():
 
     render_gui()
 
-while True:  # Continue in an infinite game loop.
 
-    console.clear()  # Blank the console.
+def play_game():
 
-    render_all()
+    while True:  # Continue in an infinite game loop.
 
-    #draw all objects in the list
-    for object in dungeon.objects:
-        obj = object.get(player)
-        if obj:
-            console.drawChar(obj.x, obj.y, obj.char)
+        console.clear()  # Blank the console.
 
-    if dungeon.player_state == 'dead':
-        status_panel.drawStr(0, 4, 'You have died!')
-    elif dungeon.player_state == 'done':
-        # pdb.set_trace()
-        status_panel.move(0, 4)
-        status_panel.printStr('CONGRADULATIONS!\n\nYou have found a Cone of Dunshire!')
-    tdl.flush()  # Update the window.
+        render_all()
 
-    for event in tdl.event.get():  # Iterate over recent events.
-        if event.type == 'KEYDOWN':
-            if dungeon.player_state == 'playing':
-                # We mix special keys with normal characters so we use keychar.
-                if event.keychar.upper() in MOVEMENT_KEYS:
-                    # Get the vector and unpack it into these two variables.
-                    keyX, keyY = MOVEMENT_KEYS[event.keychar.upper()]
-                    # Then we add the vector to the current player position.
+        #draw all objects in the list
+        for object in dungeon.objects:
+            obj = object.get(player)
+            if obj:
+                console.drawChar(obj.x, obj.y, obj.char)
 
-                    player.move_or_attack(keyX, keyY, dungeon, action_log)
+        if dungeon.player_state == 'dead':
+            status_panel.drawStr(0, 4, 'You have died!')
+        elif dungeon.player_state == 'done':
+            # pdb.set_trace()
+            status_panel.move(0, 4)
+            status_panel.printStr('CONGRADULATIONS!\n\nYou have found a Cone of Dunshire!')
 
-                    #let monsters take their turn
-                    if dungeon.player_state == 'playing':
-                        for object in dungeon.objects:
-                            if object.ai:
-                                object.ai.take_turn(dungeon, action_log)
+        tdl.flush()  # Update the window.
 
-                else:
-                    if event.keychar.upper() == 'G':
-                        #pick up an item
-                        for object in dungeon.objects:  # look for an item in the player's tile
-                            if object.x == player.x and object.y == player.y and object.item:
-                                is_cone = object.item.pickUp(dungeon.objects, action_log)
-                                if is_cone:
-                                    dungeon.player_wins(player)
-                                else:  # user picked up a health potion
-                                    player.heal_damage()
+        for event in tdl.event.get():  # Iterate over recent events.
+            if event.type == 'KEYDOWN':
+                if dungeon.player_state == 'playing':
+                    # We mix special keys with normal characters so we use keychar.
+                    if event.keychar.upper() in MOVEMENT_KEYS:
+                        # Get the vector and unpack it into these two variables.
+                        keyX, keyY = MOVEMENT_KEYS[event.keychar.upper()]
+                        # Then we add the vector to the current player position.
 
-        if event.type == 'QUIT':
-            # Halt the script using SystemExit
-            raise SystemExit('The window has been closed.')
+                        player.move_or_attack(keyX, keyY, dungeon, action_log)
+
+                        #let monsters take their turn
+                        if dungeon.player_state == 'playing':
+                            for object in dungeon.objects:
+                                if object.ai:
+                                    object.ai.take_turn(dungeon, action_log)
+
+                    else:
+                        if event.keychar.upper() == 'G':
+                            #pick up an item
+                            for object in dungeon.objects:  # look for an item in the player's tile
+                                if object.x == player.x and object.y == player.y and object.item:
+                                    is_cone = object.item.pickUp(dungeon.objects, action_log)
+                                    if is_cone:
+                                        dungeon.player_wins(player)
+                                    else:  # user picked up a health potion
+                                        player.heal_damage()
+
+            if event.type == 'QUIT':
+                # Halt the script using SystemExit
+                raise SystemExit('The window has been closed.')
+
+show_main_menu()
