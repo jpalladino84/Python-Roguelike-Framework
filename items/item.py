c00d1c2ca4e70cb965a238c310c5be156e8cc5dc
nethys <arzhul@gmail.com>
2016-11-16 20:38:48 -0500

    Some notes on what an item should be.

--- items/item.py
+++ items/item.py
@@ -1,4 +1,13 @@
 class Item(object):
+    """
+    What is an item?
+    It made out of a material, it has a type, it can be used.
+    It has stats, it can be destroyed, displayed, held.
+    It can have a rarity, a level, value.
+    It can be sharpened, altered, destroyed and repaired.
+    Painted, customized, engraved.
+    A good loot system can go a long way in terms of prolonging gameplay.
+    """
     def __init__(self):
         self.name = ""
         self.description = ""
