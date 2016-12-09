b4e534fd4bda4b2c195093d6488c226d0176f30c
ChrisLR <arzhul@gmail.com>
2016-12-09 13:07:41 -0500

    Added support for multiple pages of keybound items. Still a few quirks to iron out.

--- components/inventory.py
+++ components/inventory.py
@@ -23,26 +23,51 @@ class Inventory(object):
 
 
 class KeyBoundInventory(Inventory):
+    # noinspection SpellCheckingInspection
+    CHARACTER_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789°!\"#$%?&*()_+^:'.|{}[]@"
     """
     This inventory keeps ascii bindings to added items.
     Used for the player.
     """
     def __init__(self):
         super(KeyBoundInventory, self).__init__()
-        self._unassigned_symbols = [chr(i) for i in range(127)]
-        self._assigned_symbols = {}
+        self._page = 0
+        self._max_page = 0
+        self._unassigned_symbols = {0: list(KeyBoundInventory.CHARACTER_SET)}
+        self._assigned_symbols = {0: {}}
 
     def add_item(self, item):
         if item not in self._item_stacks:
-            self._assigned_symbols[self.__return_next_assigned_symbol()] = item.uid
+            super(KeyBoundInventory, self).add_item(item)
+            next_symbol = self.__return_next_assigned_symbol()
+            self._assigned_symbols[self._page][next_symbol] = item.uid
+            return next_symbol
+
         super(KeyBoundInventory, self).add_item(item)
+        return self.get_symbol_from_uid(item.uid)
+
+    def get_symbol_from_uid(self, item_uid):
+        return next(
+            (symbol for page, symbols in self._assigned_symbols.items()
+             for symbol, uid in symbols.items() if uid == item_uid))
 
     def pop_item_from_symbol(self, symbol):
-        self._unassigned_symbols.append(symbol)
-        return self.pop_item(self._assigned_symbols.pop(symbol))
+        self._unassigned_symbols[self._page].append(symbol)
+        return self.pop_item(self._assigned_symbols[self._page].pop(symbol))
 
-    def get_assigned_symbols(self):
-        return {symbol: name for symbol, name in self._assigned_symbols.items()}
+    def get_assigned_symbols(self, page):
+        return {symbol: name for symbol, name in self._assigned_symbols[page].items()}
 
     def __return_next_assigned_symbol(self):
-        return self._unassigned_symbols.pop(0)
+        if not self._unassigned_symbols[self._page]:
+            page = self.__find_available_page_or_create()
+            return self._unassigned_symbols[page].pop(0)
+        return self._unassigned_symbols[self._page].pop(0)
+
+    def __find_available_page_or_create(self):
+        for page, available_symbols in self._unassigned_symbols.items():
+            if available_symbols:
+                return page
+        self._max_page += 1
+        self._unassigned_symbols[self._max_page] = list(self.CHARACTER_SET)
+        return self._max_page
--- tests/components/test_inventory.py
+++ tests/components/test_inventory.py
@@ -12,3 +12,19 @@ def test_inventory_add():
     assert inventory.pop_item("cool_item_1") == cool_item_1
     assert inventory.pop_item("cool_item_1") is None
     assert inventory.pop_item("cool_item_2") == cool_item_2
+
+
+def test_inventory_keybound():
+    inventory = KeyBoundInventory()
+    cool_item_1 = Item(uid="cool_item_1", name="Very Cool Item", health=9000)
+    cool_item_2 = Item(uid="cool_item_2", name="Very Cool Item Too", health=9000)
+    cool_item_1_symbol = inventory.add_item(cool_item_1)
+    cool_item_2_symbol = inventory.add_item(cool_item_2)
+
+    assert inventory.pop_item_from_symbol(cool_item_1_symbol) == cool_item_1
+    assert inventory.pop_item_from_symbol(cool_item_2_symbol) == cool_item_2
+
+    symbols = []
+    for kek in range(0, 1000):
+        symbols.append(inventory.add_item(cool_item_1))
+    print(symbols)
