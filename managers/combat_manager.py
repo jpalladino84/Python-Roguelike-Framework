f7bfe548d3c5bce3aa09af8b8163a4f1416d8cfb
ChrisLR <arzhul@gmail.com>
2017-01-17 12:51:10 -0500

    Some notes about combat, to plan.

---
+++ managers/combat_manager.py
@@ -0,0 +1,24 @@
+
+
+# TODO This combat manager will need to handle multiple scenarios.
+# Anything could target anything, basically.
+# If we want to have good messages tying together we will need to use many enums.
+# Types could be Melee, Ranged, Magic
+# Subtypes would vary but they should include types like, Melee - Sword, Melee - Unarmed, Ranged - Arrow, Magic - Bolt
+# We should also have a list of attacks and defenses for each type and they should be limited to a particular
+# skill level.
+
+# The target must also be taken into account, being able to block, dodge, counter.
+# Can't counter a Ranged attack with Melee defense, for example.
+
+# Different maneuvers would use different stats, a dodge could use dex, block use str and magic defense could be wisdom.
+
+# One attack should only be vulnerable to a single counter.
+# We could also have finishers to execute in gory details, a small chance to happen when the target is low hp,
+# but could also be triggered manually by attacking an unconscious creature.
+
+# In all this, we should also support channeled attacks, ki beams anyone?
+# This channeling would be a sort of sustained attack.
+
+# Any limb attacked should be able to be crippled and all cripples will vary depending on damage type.
+# Like that, severed limbs would bleed out, crushed and burnt limbs would inflict massive pain.
