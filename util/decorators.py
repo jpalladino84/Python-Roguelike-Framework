3bbd734e8091d49ac6e4f682bbcd58ee00c05927
nethys <arzhul@gmail.com>
2017-04-20 13:40:45 -0400

    Removed a few unnecessary prints/logs

--- util/decorators.py
+++ util/decorators.py
@@ -1,8 +1,4 @@
 import itertools
-import logging
-
-
-logger = logging.getLogger()
 
 
 def cached(func):
@@ -12,10 +8,7 @@ def cached(func):
             setattr(host, 'cache', {})
 
         if func not in host.cache:
-            logger.debug("Setting new cache for function {}".format(func))
             host.cache[func] = func(*args, **kwargs)
-        else:
-            logger.debug("Retrieving from cache for function {}".format(func))
 
         return host.cache[func]
 
@@ -33,10 +26,7 @@ def cached_with_args(func):
 
         cached_args = tuple(itertools.chain(args, kwargs.values()))
         if cached_args not in host.cache[func]:
-            logger.debug("Setting new argument cache for function {}".format(func))
             host.cache[func][cached_args] = func(*args, **kwargs)
-        else:
-            logger.debug("Retrieving from cache for function {}".format(func))
 
         return host.cache[func]
 
@@ -47,7 +37,6 @@ def invalidate_cache(func):
     def wrapper(*args, **kwargs):
         host = args[0]
         if hasattr(host, 'cache'):
-            logger.debug("Invalidating cache for {}".format(func))
             host.cache = {}
         return func(*args, **kwargs)
 
