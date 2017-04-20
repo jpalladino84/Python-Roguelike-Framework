00dc63503bc3724a1be49ac38eda9bc1c3a7b906
Jeff Palladino <jpalladino@monetate.com>
2017-04-20 11:19:13 -0400

    [BUGFIX] - Block player's POV

--- areas/tile.py
+++ areas/tile.py
@@ -1,9 +1,9 @@
 class Tile(object):
-    def __init__(self, x, y, is_blocked):
+    def __init__(self, x, y, is_blocked=False):
         self.x = x
         self.y = y
         self.is_blocked = is_blocked
         self.is_explored = False
         self.is_ground = False
         self.contains_object = None
-        self.block_sight = False
+        self.block_sight = is_blocked
